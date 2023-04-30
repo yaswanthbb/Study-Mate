bc90b87b1578858f0ab577bdf85f7f0ca7a1a654
Yaswanth <byaswanth2003@gmail.com>
2023-04-30 15:36:51 +0530

    Subject Progress Bars - Create progress_bars dict -Using "for" loop iterate over each Subject from subject_chapters dict :  - Create Label and Progress Bars for each Subject  - Store all the content in progressFrame    (which is already created in gui.ui)  - Set Object names for Label and Progress Bars  - Add Alignment  - Store Progress Bars in progress_bars dict
    [Ticket: 104]

--- main.py
+++ main.py
@@ -21,8 +21,26 @@ class StudyMate(QMainWindow):
                     self.subject_chapters[subject].append(chapter)
                 else:
                     self.subject_chapters[subject] = [chapter]
+        
+        self.progress_bars = {}
 
+        y = 40
+
+        for subject,chapter in self.subject_chapters.items():
+            label = QLabel(self.progressFrame)
+            label.setGeometry(QRect(10,y,92,23))
+            label.setObjectName(f"label_{subject}")
+            label.setText(subject)
+            progressBar = QProgressBar(self.progressFrame)
+            progressBar.setGeometry((QRect(120, y, 118, 23)))
+            progressBar.setObjectName(f"progressBar_{subject}")
+
+            y += 40
+            self.progress_bars[subject] = progressBar
+            
         self.show()
+
+
 if __name__ == '__main__':
     app = QApplication([])
     window = StudyMate()
